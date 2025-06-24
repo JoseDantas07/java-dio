e5a5a040bba05957aaca7bf551eea83c2cbda02d
Jose Dantas <216606793+JoseDantas07@users.noreply.github.com>
2025-06-24 16:02:48 -0300

    sistema carro

---
+++ desafios/DPOO/Sistema_Carro/src/visual/Car.java
@@ -0,0 +1,76 @@
+package visual;
+
+public class Car {
+    public static void carInicioCenter(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | CC | || | ");
+    }
+    public static void carInicioleft(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| CC | || | || | ");
+    }
+    public static void carInicioRight(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | CC | ");
+    }
+    public static void carSegundoCenter(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | CC | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carSegundoLeft(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| CC | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carSegundoRight(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | CC | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carTerceiroCenter(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | CC | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carTerceiroLeft(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| CC | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carTerceiroRight(){
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | CC | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carQuartaCenter(){
+        System.out.println("| || | CC | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carQuartaLeft(){
+        System.out.println("| CC | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+    public static void carQuartaRight(){
+        System.out.println("| || | || | CC | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+        System.out.println("| || | || | || | ");
+    }
+}
